cdf6a61254e7e7573c6a5c4f30e23a84694c3139
fy <458344192@qq.com>
2020-11-26 20:27:22 +0800

    [Mod] 1.0

---
+++ android/src/main/java/com/hd/RNHDModule.java
@@ -0,0 +1,37 @@
+package com.hd;
+
+import android.os.Build;
+import android.util.Log;
+import android.util.TypedValue;
+import android.widget.Toast;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.LifecycleEventListener;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.common.ArrayUtils;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+import com.reader.usbdevice.DeviceLib;
+import com.reader.usbdevice.DeviceStatusCallback;
+
+public class RNHDModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
+    // 可以修饰成员变量和成员方法被其修饰的成员只能在本类中被访问
+    private final ReactApplicationContext reactContext;
+    private HDCardReader HDCardReader = null;
+    private DeviceLib mdev = null;
+
+    public RNHDModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        // this.reactContext = reactContext;
+        reactContext.addLifecycleEventListener(this);
+    }
+    @ReactMethod
+    public void openDevice(Boolean isConnected) {
+       mdev = new DeviceLib(getApplicationContext(), new DeviceStatusCallback(){  })
+    }
+
---
+++ index.js
@@ -0,0 +1,8 @@
+
+import { NativeModules,NativeEventEmitter } from 'react-native';
+export default {
+/**
+ * 导出模块设置监听方法回调
+ */
+
+};
