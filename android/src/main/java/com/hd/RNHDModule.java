8594ddc439ccb704ca58b9ca9c117dad76261a5b
fy <458344192@qq.com>
2020-12-02 17:24:09 +0800

    [Mod] tag 0.6

--- android/src/main/java/com/hd/RNHDModule.java
+++ android/src/main/java/com/hd/RNHDModule.java
@@ -1,26 +1,28 @@
 package com.hd;
 
-import android.os.Build;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
 import android.util.Log;
-import android.util.TypedValue;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.LifecycleEventListener;
-import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.common.ArrayUtils;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
-
 import com.reader.usbdevice.DeviceLib;
 import com.reader.usbdevice.DeviceStatusCallback;
 
 public class RNHDModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
-    // 可以修饰成员变量和成员方法被其修饰的成员只能在本类中被访问
     private final ReactApplicationContext reactContext;
     private DeviceLib mdev;
 
@@ -29,6 +31,7 @@ public class RNHDModule extends ReactContextBaseJavaModule implements LifecycleE
         this.reactContext = reactContext;
         reactContext.addLifecycleEventListener(this);
     }
+
     @Override
     public void onHostResume() {
         // Activity `onResume`
